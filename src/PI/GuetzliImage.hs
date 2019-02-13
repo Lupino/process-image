81d026486e7b596f83ff05d30c7528e6b21ee8d2
Lupino <lmjubuntu@gmail.com>
2019-02-13 15:09:28 +0800

    add lock for guetzli

--- src/PI/GuetzliImage.hs
+++ src/PI/GuetzliImage.hs
@@ -12,9 +12,10 @@ import           Control.Monad.IO.Class (liftIO)
 import           Data.Aeson             (FromJSON, parseJSON, withObject, (.!=),
                                          (.:?))
 import           Data.Int               (Int64)
+import           Data.String            (fromString)
 import           Periodic.Client        (ClientEnv, runClientT, submitJob)
-import           Periodic.Job           (JobT, name, workDone)
-import           Periodic.Types         (JobName (..))
+import           Periodic.Job           (JobT, name, withLock, workDone)
+import           Periodic.Types         (JobName (..), LockName (..))
 import           PI.Utils
 import           System.Exit            (ExitCode (..))
 import           System.FilePath        (takeFileName, (</>))
@@ -24,6 +25,8 @@ import           System.Process         (rawSystem)
 data GuetzliConfig = GuetzliConfig { guetzliCommand :: FilePath
                                    , guetzliOutput  :: FilePath
                                    , guetzliDelay   :: Int64
+                                   , guetzliLName   :: Maybe String
+                                   , guetzliLCount  :: Int
                                    }
   deriving (Show)
 
@@ -32,16 +35,20 @@ instance FromJSON GuetzliConfig where
     guetzliCommand <- o .:? "command" .!= "guetzli"
     guetzliOutput  <- o .:? "output"  .!= "guetzli"
     guetzliDelay   <- o .:? "delay"   .!= 432000
+    guetzliLName   <- o .:? "lock-name"
+    guetzliLCount  <- o .:? "lock-count" .!= 1
     return GuetzliConfig {..}
 
 defaultGuetzliConfig :: GuetzliConfig
 defaultGuetzliConfig = GuetzliConfig { guetzliCommand = "guetzli"
                                      , guetzliOutput = "guetzli"
                                      , guetzliDelay = 432000
+                                     , guetzliLName = Nothing
+                                     , guetzliLCount = 1
                                      }
 
-guetzliImage :: GuetzliConfig -> ClientEnv -> FilePath -> JobT IO ()
-guetzliImage GuetzliConfig{..} env0 root = do
+guetzliImage' :: GuetzliConfig -> ClientEnv -> FilePath -> JobT IO ()
+guetzliImage' GuetzliConfig{..} env0 root = do
   fn <- name
   let outFileName = guetzliOutput </> takeFileName fn
       outFileName' = JobName $ packBS outFileName
@@ -54,3 +61,9 @@ guetzliImage GuetzliConfig{..} env0 root = do
         void $ submitJob "upload" outFileName' Nothing Nothing
         void $ submitJob "remove" outFileName' Nothing $ Just guetzliDelay
       workDone
+
+guetzliImage :: GuetzliConfig -> ClientEnv -> FilePath -> JobT IO ()
+guetzliImage c@GuetzliConfig{..} env0 = f guetzliLName . guetzliImage' c env0
+  where f :: Maybe String -> JobT IO () -> JobT IO ()
+        f Nothing  = id
+        f (Just n) = withLock (LockName $ fromString n) guetzliLCount
